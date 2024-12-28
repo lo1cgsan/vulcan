8c948216716a00dce4fbd26145528870431750e2
lo1cgsan <lo1cgsan@gmail.com>
2024-12-28 21:27:51 +0100

    Poprawki tematu eCG

---
+++ content/pages/home.rst
@@ -0,0 +1,12 @@
+Spis treści
+###########
+
+:date: 2024-12-20 10:20
+:category: Vulcan
+:URL: index.html
+:save_as: index.html
+:author: Robert Bednarz
+
+1. `Założenie konta <{filename}vulcan_001.rst>`_
+2. `Konfiguracja dziennika <{filename}vulcan_002.rst>`_
+3. `Wprowadzanie planu <{filename}vulcan_003.rst>`_
--- pelicanconf.py
+++ pelicanconf.py
@@ -33,7 +33,17 @@ SOCIAL = (
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
+RELATIVE_URLS = True
+
+ARCHIVES_SAVE_AS = ''
+AUTHOR_SAVE_AS = ''
+AUTHORS_SAVE_AS = ''
+CATEGORY_SAVE_AS = ''
+CATEGORIES_SAVE_AS = ''
+TAGS_SAVE_AS = ''
 
 LOAD_CONTENT_CACHE = False
+CACHE_CONTENT = False
 THEME = "eCG"
+
+
